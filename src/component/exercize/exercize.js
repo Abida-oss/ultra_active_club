28ef43e43b34aac712e71ea919a5fe8ab6eb2138
Abida-oss <abidaakther0099@gmail.com>
2022-09-29 01:45:52 +0600

    start

---
+++ src/component/exercize/exercize.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const exercize = () => {
+    const exercize = [
+        {
+            
+        }
+    ]
+    return (
+        <div>
+
+            
+        </div>
+    );
+};
+
+export default exercize;
