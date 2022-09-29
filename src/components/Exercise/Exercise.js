76c2c3c8c55bae223cbb62ad17cf0b16f79282e8
Abida-oss <abidaakther0099@gmail.com>
2022-09-29 22:35:22 +0600

    profile

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -4,23 +4,82 @@ import "./Exercise.css";
 
 const Exercise = () => {
   const [fdata, setFdata] = useState([]);
+  const [list, setList] = useState([]);
+
   useEffect(() => {
     fetch("fdata.json")
       .then(res => res.json())
       .then(data => setFdata(data));
   }, []);
+
+  const handleClick = fsdata => {
+      console.log(fsdata);
+      const newList = [...list, fsdata]
+      setList(newList);
+  };
+
   return (
     <div className="main">
       <div className="exercise-container">
         {fdata.map(fsdata => (
-            <Fdata
-                key={fsdata.id}
-                fsdata={fsdata}
-            ></Fdata>
+          <Fdata
+            key={fsdata.id}
+            fsdata={fsdata}
+            handleClick={handleClick}
+          ></Fdata>
         ))}
       </div>
       <div className="cart-container">
-        <h1>hello</h1>
+        <div className="profile">
+          <h2>Abeda </h2>
+          <h4>Sylhet</h4>
+          <div className="about">
+            <div>
+              <h3>
+                50
+                <small style={{ color: "black", fontWeight: "normal" }}>
+                  kg
+                </small>
+              </h3>
+              <p>Weight</p>
+            </div>
+            <div>
+              <h3>5.1</h3>
+              <p>Height</p>
+            </div>
+            <div>
+              <h3>
+                24
+                <small style={{ color: "black", fontWeight: "normal" }}>
+                  year
+                </small>
+              </h3>
+              <p>Age</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Add A Break</h3>
+          <div className="break">
+            <p>10s</p>
+            <p>20s</p>
+            <p>30s</p>
+            <p>40s</p>
+            <p>50s</p>
+          </div>
+        </div>
+        <div>
+          <h3>Exercize Details</h3>
+          <div className="details">
+            <p style={{ fontWeight: "bold" }}>
+              Exercize time <span style={{ paddingRight: 50}}></span>
+              {list.length}
+            </p>
+          </div>
+          <div className="details">
+            <p style={{ fontWeight: "bold" }}>Break time </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Fdata/Fdata.js
+++ src/components/Fdata/Fdata.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Fdata.css';
 
 const Fdata = (props) => {
-    const {name, img, age, time} = props.fsdata;
+    const { name, img, age, time } = props.fsdata;
+    
 
     return (
       <div className="card">
         <img src={img} alt=""></img>
         <div className='e-info'>
-          <p className="e_name">Name: {name}</p>
+          <p>Name: {name}</p>
           <p>Age: {age}</p>
           <p>Time: {time}min</p>
             </div>
-            <button className='btn'>
+            <button onClick={() => props.handleClick(props.fsdata)} className='btn'>
                 <p>Add to list</p>
             </button>
       </div>
